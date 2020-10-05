b017b65e82cbc55c8d81fd026f870aaf6db7a91c
taketya888 <65265504+taketya888@users.noreply.github.com>
2020-10-05 10:07:06 +0900

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,59 @@
+<template>  
+  <div>
+    <h1>Todo List</h1>
+    <input type="text" v-model="newTask">
+    <input type="button" value="追加" v-on:click="addTask">
+    <div class="list">
+      <ul>
+        <li v-for="item in list" :key="item.id">
+          <span :class="{ complete: item.isComplete }">{{item.value }}</span>
+          <input type="button" value="達成" v-on:click="item.isComplete=true">
+          <input type="button" value="取り消し" v-on:click="item.isComplete=false">
+          <input type="button" value="削除">
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      list: [
+        {id: 1, value: "靴をおろす", isComplete: false},
+        {id: 2, value: "風呂のカビ取り", isComplete: false},
+        {id: 3, value: "電気を消して寝る", isComplete: false}
+      ],
+      newTask: "",
+      nextId:  4
+    } 
+  },
+  methods: {
+    addTask() {
+      if(!this.newTask.trim()) {
+        return
+      }
+      this.list.push({
+        id: this.nextId++,
+        value: this.newTask,
+        isComplete: false
+      })
+      this.newTask = ""
+    }
+  }
+}
+</script>
+
+<style>
+  .list {
+    width: 60%;
+    margin: auto;
+    text-align: left;
+  }
+
+  .complete {
+    text-decoration: line-through;
+  }
+</style>
